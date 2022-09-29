98eb490ddc43590ec1111bacb108edc22bcecbeb
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-29 10:45:02 -0500

    DB file complete and /search route setup

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// Import api key
+require('dotenv').config();
+
 const app = express();
 // App PORT set with production check
 const PORT = process.env.PORT || 5000;
@@ -8,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 // Route includes
 const favoriteRouter = require('./routes/favorite.router');
 const categoryRouter = require('./routes/category.router');
+const searchRouter = require('./routes/search.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -19,6 +23,7 @@ app.use(express.static('build'));
 // Routes
 app.use('/api/favorite', favoriteRouter);
 app.use('/api/category', categoryRouter);
+app.use('/search', searchRouter);
 
 // Listen
 app.listen(PORT, () => {
